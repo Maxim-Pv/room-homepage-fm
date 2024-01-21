3ffb114588247478984939ee4def0e668689013f
Maksim Pavlov <maxim.paw2010@yandex.ru>
2024-01-21 19:56:56 +0300

    add all files

---
+++ main.js
@@ -0,0 +1,64 @@
+const backgroundMenu = document.querySelector('.section-menu')
+const arrows = document.querySelectorAll('.arrow')
+const content = document.querySelectorAll('.section-shop__content')
+let value = 0
+
+function show(index) {
+    backgroundMenu.classList.remove('background-0', 'background-1', 'background-2');
+    backgroundMenu.classList.add(`background-${index}`);
+
+    content[value].classList.remove('active');
+    content[index].classList.add('active');
+    value = index;
+}
+
+arrows.forEach((arrow) => {
+    arrow.addEventListener('click', (event) => {
+        let index
+        if (event.currentTarget.classList.contains('prev')) {
+            index = value - 1;
+            if (index < 0) {
+                index = 2;
+            }
+        } else if (event.currentTarget.classList.contains('next')) {
+            index = value + 1;
+            if (index > 2) {
+                index = 0;
+            }
+        }
+        show(index)
+    })
+})
+
+
+document.addEventListener('keydown', (event) => {
+    const code = event.code;
+    switch (code) {
+        case 'ArrowLeft':
+            navigate('prev');
+            break;
+        case 'ArrowRight':
+            navigate('next');
+            break;
+    }
+})
+
+function navigate(direction) {
+    let index;
+    if (direction === 'prev') {
+        index = value - 1;
+        if (index < 0) {
+            index = 2;
+        }
+    } else if (direction === 'next') {
+        index = value + 1;
+        if (index > 2) {
+            index = 0;
+        }
+    }
+    show(index);
+}
+
+
+
+show(value)
